f1d6669996d8561569ea427c43805202b45c68da
JieCHERY <9594523wrj@gmail.com>
2025-01-09 18:42:13 +0800

    collection

---
+++ a-ds-algorithms/datasource/src/jdk.jdeps/com/sun/tools/jdeps/resources/version.java
@@ -0,0 +1,11 @@
+package com.sun.tools.jdeps.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "17.0.13+10-LTS-268" },
+            { "jdk", "17.0.13" },
+            { "release", "17.0.13" },
+        };
+    }
+}
